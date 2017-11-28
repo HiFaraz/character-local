46145f0c0ff35bc98f9d77931744ea9c3fe9f029
Faraz Syed <github@services.farazsyed.com>
2017-11-28 00:20:14 -0700

    Refactor local authentication into separate repo

---
+++ src/index.js
@@ -0,0 +1,192 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+import { INTERNAL_SERVER_ERROR, SEE_OTHER, UNAUTHORIZED } from 'http-codes';
+import Authenticator from 'character-authenticator/post';
+// import PassportAdapter from 'character-passport/post';
+import asyncpipe from 'asyncpipe';
+import models from './models';
+
+// TODO make this a separate module that can be used to apply Passport strategies
+void class PassportStrategyAuthenticator {
+  async authenticate({ req, res }) {
+    return new Promise((resolve, reject) => {
+      if (!this.strategy) {
+        const error = new Error('Passport strategy not defined');
+        error.httpStatusCode = INTERNAL_SERVER_ERROR;
+        return reject(error);
+      }
+
+      const strategy = Object.create(this.strategy);
+
+      /**
+       * Augment the strategy
+       */
+
+      strategy.error = function(error) {
+        error.httpStatusCode = error.httpStatusCode || INTERNAL_SERVER_ERROR;
+        reject(error);
+      };
+
+      strategy.fail = function(challenge, status = UNAUTHORIZED) {
+        const _status = typeof challenge === 'number' ? challenge : status;
+
+        const error = new Error('Unable to authenticate');
+        error.httpStatusCode = _status;
+        reject(error);
+      };
+
+      // redirects should only happen on a GET request based authenticator
+      // NOTE: this function is taken from Passport.js code
+      strategy.redirect = function(url, status) {
+        // NOTE: Do not use `res.redirect` from Express, because it can't decide
+        //       what it wants.
+        //
+        //       Express 2.x: res.redirect(url, status)
+        //       Express 3.x: res.redirect(status, url) -OR- res.redirect(url, status)
+        //         - as of 3.14.0, deprecated warnings are issued if res.redirect(url, status)
+        //           is used
+        //       Express 4.x: res.redirect(status, url)
+        //         - all versions (as of 4.8.7) continue to accept res.redirect(url, status)
+        //           but issue deprecated versions
+
+        res.statusCode = status || 302;
+        res.setHeader('Location', url);
+        res.setHeader('Content-Length', '0');
+        res.end();
+        // resolve(); // TODO is this required?
+      };
+
+      strategy.success = function(user, info = {}) {
+        resolve({ account: Object.assign({ _info: info }, user), req, res });
+      };
+
+      strategy.authenticate(req, res);
+    });
+  }
+};
+
+module.exports = class LocalAuthenticator extends Authenticator {
+  /**
+   * Handles requests from the hub to the authenticator
+   *
+   * @param {Object} context
+   * @param {IncomingMessage} context.req
+   * @param {ServerResponse} context.res
+   * @return {Promise<Object>}
+   */
+  async authenticate({ req, res }) {
+    // errors are handled upstream by the Authentication plugin
+    return {
+      account: await this.models.User.authenticate(
+        req.body.username,
+        req.body.password,
+      ),
+      req,
+      res,
+    };
+  }
+
+  /**
+   * Define extra authenticator routes
+   */
+  extend() {
+    // Add open registration route if enabled
+    if (this.config.registrationOpen) {
+      const registrationPath = '/register';
+
+      // session middleware needed if login after registration enabled
+      if (this.config.loginAfterRegistration) {
+        this.router.post(registrationPath, this.deps.session);
+      }
+
+      // add registration middleware
+      this.router.post(registrationPath, async (req, res, next) => {
+        try {
+          await asyncpipe(register, onboard, login, () =>
+            res.redirect(
+              SEE_OTHER,
+              this.config.registrationRedirect || this.config.successRedirect,
+            ),
+          )({
+            User: this.models.User,
+            authenticator: this,
+            req,
+            res,
+          });
+        } catch (error) {
+          return res.redirect(
+            SEE_OTHER,
+            this.config.registrationFailureRedirect ||
+              this.config.failureRedirect,
+          );
+        }
+      });
+    }
+  }
+
+  static defaults() {
+    return {
+      loginAfterRegistration: true,
+      registrationOpen: true,
+    };
+  }
+
+  static models() {
+    return models;
+  }
+};
+
+/**
+ * Login the user after registration if enabled
+ *
+ * @param {Object} context
+ * @return {Promise<Object>}
+ */
+async function login(context) {
+  // login the user if enabled
+  if (context.authenticator.config.loginAfterRegistration) {
+    context.req.login({
+      authenticator: {
+        account: { id: context.account.id },
+        name: context.authenticator.name,
+      },
+      id: context.identity.id,
+    }); // TODO feels like I have to remember a complex interface whenever I want to login, fix this
+  }
+  return context;
+}
+
+/**
+ * Onboard the authenticator account by creating a new core identity
+ *
+ * @param {Object} context
+ * @return {Promise<Object>}
+ */
+async function onboard(context) {
+  const identity = await context.authenticator.onboard({
+    id: context.account.id,
+    username: context.account.username, // the function doesn't need this, but it is passed so that it gets emitted for auditing
+  }); // create a new core identity
+  return Object.assign({ identity }, context);
+}
+
+/**
+ * Register username and password
+ *
+ * @param {Object} context
+ * @return {Promise<Object>}
+ */
+async function register(context) {
+  const account = await context.User.create({
+    password: context.req.body.password,
+    username: context.req.body.username,
+  });
+  return Object.assign(
+    { account: { id: account.id, username: context.req.body.username } },
+    context,
+  );
+}
